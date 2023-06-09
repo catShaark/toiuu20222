c9b1d4a067278a4c1b504ab62891a2d188a235af
DongLieu <duongduydongtq@gmail.com>
2023-06-09 15:32:05 +0700

    ok GA

--- GA/code/GA.py
+++ GA/code/GA.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import copy
 import time
+import random
 
 import Solution
 
@@ -10,6 +11,7 @@ class Ga():
         self.Gen = Gen
         self.n_pop = N
         self.remove = remove
+        self.mutation_rate = 0.8
 
         self.pop = []
         self.fitness = []
@@ -38,19 +40,117 @@ class Ga():
             # sap xep, chon loc
             self.sort_selective()
     def reproductionss(self):
-        # lai 
         child = []
-        child_tmp, err = self.laighep()
+        # lai 
+        for i in range(self.n_pop):
+            if i in self.expulsion_set:continue
+            for j in range(i, self.n_pop):
+                if j in self.expulsion_set:continue
+                child_tmp, suc = self._laighep(i,j)
+                if suc:
+                    child = child + child_tmp
 
         # dot bien
-        child_tmp2, err = self.dotbien()
+        for i in range(self.n_pop):
+            if i in self.expulsion_set:continue
+            child_tmp2, suc = self._dotbien(i)
+            if suc:
+                child = child + [child_tmp2]
+        # trung
+        childs_return = []
+        for chi in child:
+            if self._sol_not_in_pop(chi) == True:
+                if self._not_in(chi, childs_return) == True:
+                    childs_return.append(chi)
 
-        add = 0
-        if len(child) < len(self.expulsion_set):
-            add = len(self.expulsion_set) - len(child)
+        for i in childs_return:
+            print("dotuongdong1:{}| x:{} |y:{}".format(i.dotuongdong, i.x, i.y))
+
         # init
+        start_time = time.time()
+        while(len(childs_return) < len(self.expulsion_set)):
+            current_time = time.time()  # Lấy thời gian hiện tại
+            elapsed_time = current_time - start_time  # Tính thời gian đã trôi qua
+            if elapsed_time >= 12:  # Kiểm tra nếu đã đạt đến thời gian kết thúc (ví dụ: 60 giây - 1 phút)
+                for hi in self.expulsion_set:
+                    sol_new = copy.deepcopy(self.pop[hi])
+                    childs_return.append(sol_new)
+                    if len(childs_return) == len(self.expulsion_set):
+                        break  # Thoát khỏi vòng lặp
 
+            sol_init = copy.deepcopy(self.sol_sample)
+            sol_init.init_Sol()
+            if (sol_init.rang_buoc()) and (self._sol_not_in_pop(sol_init)):
+                childs_return.append(sol_init)
+            else:
+                del sol_init
+
+        for i_sol in self.expulsion_set:
+            element_to_remove = random.choice(childs_return)
+            self.pop[i_sol] = element_to_remove
+            childs_return.remove(element_to_remove)
         
+    def _laighep(self,sol1_id, sol2_id):
+        child = []
+
+        parent1_x = copy.deepcopy(self.pop[sol1_id].x)
+        parent1_y = copy.deepcopy(self.pop[sol1_id].y)
+
+        parent2_x = copy.deepcopy(self.pop[sol2_id].x)
+        parent2_y = copy.deepcopy(self.pop[sol2_id].y)
+
+        # Chọn một điểm cắt ngẫu nhiên
+        crossover_point = random.randint(1, len(parent1_x)-1)
+        
+        # Lai ghép chuỗi gen từ cha mẹ
+        child1_x = parent1_x[:crossover_point] + parent2_x[crossover_point:]
+        child1_y = parent1_y[:crossover_point] + parent2_y[crossover_point:]
+        
+        child2_x = parent2_x[:crossover_point] + parent1_x[crossover_point:]
+        child2_y = parent2_y[:crossover_point] + parent1_y[crossover_point:]
+
+        sol_child1 = copy.deepcopy(self.sol_sample)
+        sol_child2 = copy.deepcopy(self.sol_sample)
+
+        sol_child1.x = child1_x
+        sol_child1.y = child1_y
+        sol_child1.tinhk_xy()
+
+        if sol_child1.rang_buoc():
+            child.append(sol_child1)
+
+        sol_child2.x = child2_x
+        sol_child2.y = child2_y
+        sol_child2.tinhk_xy()
+
+        if sol_child2.rang_buoc():
+            child.append(sol_child2)
+        
+        return child, (child != []) 
+    
+    def _dotbien(self, sol_id):
+
+        mutated_x = copy.deepcopy(self.pop[sol_id].x)
+        mutated_y = copy.deepcopy(self.pop[sol_id].y)
+        
+        for i in range(len(mutated_x)):
+            if random.random() < self.mutation_rate:
+                random_number = random.randint(0, self.sol_sample.K -1)
+                mutated_x[i] = random_number  # Đột biến bit i của gen x
+
+            if i < len(mutated_y):
+                if random.random() < self.mutation_rate:
+                    random_number = random.randint(0, self.sol_sample.K -1)
+                    mutated_y[i] = random_number  # Đột biến bit i của gen y
+                
+        sol_child = copy.deepcopy(self.sol_sample)
+        sol_child.x = mutated_x
+        sol_child.y = mutated_y
+        sol_child.tinhk_xy()
+        if sol_child.rang_buoc():
+            return sol_child, True
+        else:
+            return None, False
     
     def print_gen(self, gen):
         with open(self.path_output, 'a') as file:
@@ -76,7 +176,7 @@ class Ga():
         while(len(self.pop) <= self.n_pop):
             current_time = time.time()  # Lấy thời gian hiện tại
             elapsed_time = current_time - start_time  # Tính thời gian đã trôi qua
-            if elapsed_time >= 6:  # Kiểm tra nếu đã đạt đến thời gian kết thúc (ví dụ: 60 giây - 1 phút)
+            if elapsed_time >= 12:  # Kiểm tra nếu đã đạt đến thời gian kết thúc (ví dụ: 60 giây - 1 phút)
                 self.n_pop = len(self.pop)
                 break  # Thoát khỏi vòng lặp
 
@@ -87,9 +187,15 @@ class Ga():
             else:
                 del sol_init
                 
-    def _sol_not_in_pop(self, sol:Solution) -> bool:
-        for sol_id in self.pop:
-            if (sol_id.x == sol.x) and (sol_id.y == sol.y):
+    def _sol_not_in_pop(self, sol_test:Solution):
+        for sol_i in self.pop:
+            if (sol_i.x == sol_test.x):
+                if (sol_i.y == sol_test.y):
+                    return False
+        return True
+    def _not_in(self, chi, chi_return):
+        for c in chi_return:
+            if (chi.x == c.x) and (chi.y == c.y):
                 return False
         return True
         
